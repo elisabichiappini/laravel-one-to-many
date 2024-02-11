cc58fc60e84b1f600c831f8398660c450cf1de94
elisabichiappini <elisabichiappini@gmail.com>
2024-02-11 21:15:52 +0100

    feat: field type_id e fk

---
+++ database/migrations/2024_02_11_194551_add_field_type_id_foreing_table_projects.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function(Blueprint $table) {
+            //creo colonna e mi lego da project a types
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
+            //la definisco foreign key, quindi dentro il campo di type_id mi riferisco all'id di types 
+            $table->foreign('type_id')->references('id')->on('types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function(Blueprint $table) {
+            //cancelliamo la foreign key
+            $table->dropForeign('projects_type_id_foreign');
+            //cancelliamo anche la colonna
+            $table->dropColumn('type_id');
+        });
+    }
+};
