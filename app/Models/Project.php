936aa08043d1fc8bc8c4d0bc14d6ee12a6124789
elisabichiappini <elisabichiappini@gmail.com>
2024-02-12 23:05:57 +0100

    feat: relation in project technology

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,12 @@ class Project extends Model
     use HasFactory;
     
     protected $guarded = ['slug', 'project_img'];
+    
+    public function type() {
+        return $this->belongsTo(Type::class);
+    }
+
+    public function technologies() {
+        return $this->belongsToMany(Technology::class);
+    }
 }
