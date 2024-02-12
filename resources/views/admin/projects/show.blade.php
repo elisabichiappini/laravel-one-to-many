99d41c342635808596e2cb8fdf3ecde55fd182cb
elisabichiappini <elisabichiappini@gmail.com>
2024-02-13 00:45:20 +0100

    feat: show in create page

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -15,11 +15,37 @@
 @section('content')
     <!--elementi in vista del singolo progetto-->
     <h2>{{ $project->title }}</h2>
+    <!--elemento type-->
+    <div>
+        <span class="badge text-bg-info">
+            Categoria: {{ $project->type?->title ?: 'Nessuna categoria' }}
+        </span>
+    </div>
+    <!--/elemento type-->
+    <!--elemento tecnologia-->
+    <div>
+        <h5>Tags</h5>
+        <ul>   
+            @foreach ( $project->technologies as $technology )
+            <li>
+                @if($technology) 
+                <span class="badge text-bg-warning">{{ $technology->title }}</span>
+                @else 
+                <span class="badge text-bg-danger">Nessuna tecnologia attribuita</span>
+                @endif
+            </li>
+            @endforeach
+        </ul>
+    </div>
+    <!--/elemento tecnologia-->
     <h6>{{ $project->born }}</h6>
     <h3>{{ $project->tools }} </h3>
+    
     <p>{{ $project->description}}</p>
     @if ($project->project_img)
         <img src="{{ asset('storage/' . $project->project_img) }}" alt="{{ $project->title }}">
     @endif
     <!--elementi in vista del singolo progetto-->
+
+    
 @endsection
