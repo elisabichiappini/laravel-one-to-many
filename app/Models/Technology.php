be1dfb08e67863802c8545b154a43046085ef508
elisabichiappini <elisabichiappini@gmail.com>
2024-02-13 19:09:02 +0100

    feat: guarded project in model technology

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Technology extends Model
 {
     use HasFactory;
+    // se pensiamo di passare progetti alle tecnologie nelle crud
+    protected $guarded = ['project'];
 
     public function projects() {
         return $this->belongsToMany(Project::class);
