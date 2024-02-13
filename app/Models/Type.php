23923ca2ec3206f5f0fb98360c8987be3b73354b
elisabichiappini <elisabichiappini@gmail.com>
2024-02-13 19:27:17 +0100

    feat: relation model type

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
+    //relazione una categoria può appartenere a un progetto
+    public function projects() {
+        return $this->hasOne(Project::class);
+    }
 }
